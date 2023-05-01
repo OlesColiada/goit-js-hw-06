bce6e489fe7b5d8e03ec3b0424662002de59baec
OlesColiada <olescoliada@gmail.com>
2023-05-01 17:39:04 +0300

    виправив перше завдання

--- js/task-01.js
+++ js/task-01.js
@@ -4,9 +4,10 @@ console.log("Number of categories:" , categoriesList.length)
 console.log('')
 
 const newArrayChildren = [...categoriesList]
+console.log(newArrayChildren)
 newArrayChildren.forEach(category => {
-    const localTitle = category.querySelector('h2').textContent;
-    const quantityOfLi = category.querySelectorAll('li').length;
+    const localTitle = category.firstElementChild.textContent;
+    const quantityOfLi = category.lastElementChild.children.length;
     console.log('Category:', localTitle);
     console.log('Elements:',quantityOfLi);
     console.log('')
